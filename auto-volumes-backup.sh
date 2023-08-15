23fdac00c42afd62b1ad741aab076b717f64fa62
Florent <43142477+EliteWise@users.noreply.github.com>
2023-08-15 19:40:08 +0200

    Update auto-volumes-backup.sh

--- auto-volumes-backup.sh
+++ auto-volumes-backup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Inclure le fichier config
+source config.sh
+
 # Vérifier que l'utilisateur a fourni les arguments nécessaires
 if [[ $# -lt 3 ]]; then
     echo "Usage: $0 [REMOTE_USER] [REMOTE_HOST] [REMOTE_PATH]"
@@ -32,11 +35,25 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+# Assurez-vous que la clé SSH a les bonnes permissions
+chmod 600 "$SSH_KEY_PATH"
+
 # Envoyer la sauvegarde au premier poste distant
-scp $BACKUP_PATH $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+scp -i "$SSH_KEY_PATH" $BACKUP_PATH $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 if [[ $? -ne 0 ]]; then
     echo "Erreur lors de l'envoi de la sauvegarde au serveur distant."
     exit 1
 fi
 
 echo "Sauvegarde réussie et envoyée au serveur distant."
+
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+
+if [ "$SETUP_CRON" = true ]; then
+    if ! crontab -l | grep -q "path_to_your_script"; then
+        (crontab -l ; echo "$CRON_SCHEDULE /bin/bash $SCRIPT_PATH/$(basename "$0")") | crontab -
+        echo "Cron job configuré pour s'exécuter selon : $CRON_SCHEDULE"
+    else
+        echo "Cron job déjà configuré pour ce script."
+    fi
+fi
