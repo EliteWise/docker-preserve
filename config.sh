53f09a239a95b05dc91c900379a064c5f35cc6ab
Florent <43142477+EliteWise@users.noreply.github.com>
2023-08-15 19:14:08 +0200

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,11 @@
+# Configurations générales
+BACKUP_FOLDER="./backups"
+VOLUME_PATH="./volumes"
+REMOTE_PATH="/home/ubuntu/backups"
+
+# Configurations pour la connexion distante
+REMOTE_USER="root"
+REMOTE_HOST="example.com"
+
+# Configuration clé SSH (si nécessaire)
+SSH_KEY_PATH="/root/.ssh/backup-ssh"
